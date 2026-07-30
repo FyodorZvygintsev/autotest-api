3abf7ab7af4fc76b9330ac941de33057838d6603
FyodorZvygintsev <zvyagintsevf@gmail.com>
2026-07-30 16:28:09 +0400

    Знакомство с WebSocket

---
+++ websocket_users_server.py
@@ -0,0 +1,29 @@
+import asyncio
+
+import websockets
+from  websockets import ServerConnection
+
+
+async def echo(websocket: ServerConnection):
+    async for message in websocket:
+        print(f"Получено сообщение от пользователя {message}")
+        for number in range(1, 6):
+
+            response = f"{number} Сообщение пользователя: {message}"
+            await websocket.send(response)
+
+
+
+
+
+
+
+async def main():
+    server = await websockets.serve(echo, "localhost", 8765)
+    print("WebSocet сервер запущен на ws://localhost:8765")
+    await server.wait_closed()
+
+
+
+
+asyncio.run(main())
